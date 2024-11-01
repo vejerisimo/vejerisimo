d0d4ff9094154d35a99b501f4b1f836c6fc2dfe5
Koen Handekyn <github.com@handekyn.com>
2024-11-01 11:31:23 +0100

    updates

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 bundle exec middleman build
-cd build
+# cd build
 git add . --all
 git commit -am updates
-git push origin master:gh-pages
+# git push origin master:gh-pages
 git push origin master
-cd ..
+# cd ..
